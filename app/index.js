7b6360be1e6e86746ee49492bf37656db883105f
Thomas Fritz <fritztho@gmail.com>
2012-08-19 00:00:02 +0200

    Added Sample Hello World app

---
+++ app/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+
+app.get('/', function(request, response) {
+	response.end("<!DOCTYPE HTML>Hello World!");
+});
+
+server.listen(3000);
+console.log('Listening on http://localhost:3000');
