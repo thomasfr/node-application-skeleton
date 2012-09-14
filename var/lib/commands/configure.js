5780be290e2d3de1b40b53a729731ea80b86c119
Thomas Fritz <fritztho@gmail.com>
2012-09-14 17:33:24 +0200

    Modified working version of start and configure command

--- var/lib/commands/configure.js
+++ var/lib/commands/configure.js
@@ -1,32 +1,32 @@
-var fs = require('fs')
-var simpleReplace = require('simple-replace')
+var fs = require('fs');
+var simpleReplace = require('simple-replace');
 
 var configure = module.exports = function configureCommand(config, args) {
-  var confDir = config['APP_CONFPATH']
-  var files = fs.readdirSync(confDir)
+  var confDir = config['APP_CONFPATH'];
+  var files = fs.readdirSync(confDir);
 
   if (!files || files.length <= 0) {
-    return false
+    return false;
   }
 
-  var processFile = function p(fileName) {
+  var processFile = function processFile(fileName) {
     if (fileName.search(filePatternRegex) >= 0) {
-      confFileName = confDir + '/' + fileName
-      newConfFileName = confFileName.replace(filePatternRegex, "")
-      console.log("Processing config file ('" + newConfFileName + "')")
-      fileContents = fs.readFileSync(confFileName, 'utf8')
-      fileOutput = simpleReplace(fileContents, configHash)
-      fs.writeFileSync(newConfFileName, fileOutput, 'utf8')
+      confFileName = confDir + '/' + fileName;
+      newConfFileName = confFileName.replace(filePatternRegex, "");
+      console.log("Processing config file ('" + newConfFileName + "')");
+      fileContents = fs.readFileSync(confFileName, 'utf8');
+      fileOutput = simpleReplace(fileContents, configHash);
+      fs.writeFileSync(newConfFileName, fileOutput, 'utf8');
     }
-  }
+  };
 
-  var fileContents
-  var fileOutput
-  var filePatternRegex = /\.tpl$/
-  var confFileName
-  var newConfFileName
-  var configHash = process.env
+  var fileContents;
+  var fileOutput;
+  var filePatternRegex = /\.tpl$/;
+  var confFileName;
+  var newConfFileName;
+  var configHash = process.env;
 
-  files.forEach(processFile)
-  return true
+  files.forEach(processFile);
+  return true;
 };
--- var/lib/commands/start.js
+++ var/lib/commands/start.js
@@ -1,33 +1,39 @@
-var fs = require('fs')
-var spawn = require('child_process').spawn
+var fs = require('fs');
+var fork = require('child_process').fork;
 
 module.exports = function startCommand(config, args) {
   if (fs.existsSync(config['pidfile'])) {
     console.log("PID file exists ('" + config['pidfile'] + "')");
-    console.log("Maybe '" + config['APP_NAME'] + "' already running!?")
-    return false
+    console.log("Maybe '" + config['APP_NAME'] + "' already running!?");
+    return false;
   }
 
-  var cleanupOnExit = function c() {
+  fs.writeFileSync(config['pidfile'], process.pid, 'utf8');
+
+  var cleanExit = function cleanExit() {
     if (fs.existsSync(config['pidfile'])) {
-      console.log("\nRemoving PID file ('" + config['pidfile'] + "')")
-      fs.unlinkSync(config['pidfile'])
+      console.log("\nRemoving PID file ('" + config['pidfile'] + "')");
+      fs.unlinkSync(config['pidfile']);
     }
+    process.exit();
+  };
+
+  process.on('SIGKILL', cleanExit);
+  process.on('SIGTERM', cleanExit);
+  process.on('SIGINT', cleanExit);
+
+  process.chdir(config['APP_HOMEPATH']);
+
+  try {
+    var child = fork(config['APP_HOMEPATH'], [], {
+      cwd: config['APP_HOMEPATH'],
+      env: process.env
+    });
+
+  } catch (e) {
+    console.error("Could not fork app.");
+    throw new Error(e.getMessage());
   }
 
-  //args.unshift('start');
-  args.unshift(config['APP_HOMEPATH'])
-  var child = spawn('node', args, {
-    stdio: ['ignore', process.stdout, process.stderr],
-    cwd: config['APP_HOMEPATH'],
-    env: process.env
-  })
-  console.log("Started " + config['APP_NAME'] + " (pid: '" + child.pid + "')")
-
-  process.on('SIGINT', cleanupOnExit)
-  process.on('SIGQUIT', cleanupOnExit)
-  process.on('SIGKILL', cleanupOnExit)
-
-  fs.writeFileSync(config['pidfile'], child.pid)
-  return true
+  return true;
 }
